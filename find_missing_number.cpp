2dd78aa833f6c7644848c1509f3cff559f1ce7f0
Anand Pandey <anandp332@gmail.com>
2020-03-14 18:49:36 +0530

    practice problem

---
+++ find_missing_number.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    vector<int> *arr = new vector<int>(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+
+	// Write your code here
+}
