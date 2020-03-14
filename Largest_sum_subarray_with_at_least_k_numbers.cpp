fb988fdd24fbca5b7ebd5f9cb8a4a840d2980387
Anand Pandey <anandp332@gmail.com>
2020-03-14 19:44:20 +0530

    missing number

---
+++ Largest_sum_subarray_with_at_least_k_numbers.cpp
@@ -0,0 +1,26 @@
+/*
+
+Interview Shuriken 36: Largest sum subarray with at-least k numbers
+Level MEDIUM
+This problem was asked by Facebook.
+Given an array, find the subarray (containing at least k numbers) which has the largest sum. You have to print that largest sum.
+Input Format:
+In the first line of input, you are given with the array size, next line of input contains the input array and the next line gives you the value of k.
+Constraints:
+Time Limit: 1 second
+The size of array lies in the range: [1, 100000]
+The value of array elements lies in the range: [-1000, 1000]
+The value of k lies in the range: [1, size of array].
+Output Format:
+Value of largest sum for a subarray(containing at least k elements).
+Sample Input 1:
+6
+1 1 1 1 1 1
+2
+Sample Input 2:
+6
+Explanation:
+If we include all the elements of input array in the subarray, it will give us the maximum sum here. So, the subarray is:
+{1,1,1,1,1,1}
+
+*/
--- find_missing_number.cpp
+++ find_missing_number.cpp
@@ -1,3 +1,29 @@
+/*
+
+Interview Shuriken 53: Find the Missing Number
+Level EASY
+You are given an array containing ‘n’ distinct numbers taken from the range: [0, n]. Since the array contains only ‘n’ numbers and count of numbers from 0 to n is ‘n+1’, so, you have to find the missing number.
+Example 1:
+Input: [4, 0, 3, 1]
+Output: 2
+Example 2:
+Input: [8, 3, 5, 2, 4, 6, 0, 1]
+Output: 7
+Input Format:
+The first line of input contains an integer, that denotes the value of n. The following line contains n space separated integers, that denotes the value of array elements.
+Constraints:
+1 <= N <= 100000
+0 <= A[i] <= N 
+Time Limit: 0.5 seconds.
+Output Format:
+The first and only line of output contains the missing number, as described in the task.
+Sample Input 1:
+6
+1 5 6 0 3 2
+Sample Output 1:
+4
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
