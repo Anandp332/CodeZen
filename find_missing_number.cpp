fd9ccf4587cdcf76df4a822ea09b0f99c8135f81
Anand Pandey <anandp332@gmail.com>
2020-03-14 19:17:39 +0530

    missing number

--- find_missing_number.cpp
+++ find_missing_number.cpp
@@ -3,11 +3,24 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    vector<int> *arr = new vector<int>(n);
+    int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    
+    sort(arr, arr+n);
+    //cout<<"sorted array"<<endl;
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i]<<" ";
+    //}
 
+    //  for (auto x : arr) 
+    //     cout << x << " ";
+    for(int i=0;i<n;i++){
+        if(i!=arr[i]){
+        cout<<i<<endl;
+        break;
+        }
+    }
+return 0;
 	// Write your code here
 }
